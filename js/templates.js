ef99077d4f4995402a9c1dc1df2f22294ca888e1
Calvin Walton <calvin.walton@kepstin.ca>
2013-09-27 07:37:25 -0400

    Fix release tile art, add recording link disambig

--- js/templates.js
+++ js/templates.js
@@ -19,18 +19,25 @@ var templateOptions = {
 	}
 };
 
-var linkTemplate = jsontemplate.Template('<a href="{url|htmltag}"{.section title} title="{@|htmltag}"{.end}>{text|html}</a>');
+var linkTemplate = jsontemplate.Template(
+	'<a href="{url|htmltag}"{.section title} title="{@|htmltag}"{.end}>' +
+		'{text|html}' +
+	'</a>' +
+	'{.section comment}' +
+		' <span class="comment">({@|html|)</span>' +
+	'{.end}'
+);
 function formatLink(link) {	return linkTemplate.expand(link); }
 function formatArtistLink(a) { return linkTemplate.expand({ url: '?artist=' + a['id'], title: a['sort-name'], text: a['name'] }); }
 function formatReleaseLink(r) { return linkTemplate.expand({ url: '?release=' + r['id'], text: r['title'] }); }
 function formatReleaseGroupLink(rg) { return linkTemplate.expand({ url: '?release-group=' + rg['id'], text: rg['title'] }); }
 function formatLabelLink(l) { return linkTemplate.expand({ url: '?label=' + l['id'], title: l['sort-name'], text: l['name'] }); }
-function formatRecordingLink(r) { return linkTemplate.expand({ url: '?recording=' + r['id'], text: r['title'] }); }
+function formatRecordingLink(r) { return linkTemplate.expand({ url: '?recording=' + r['id'], text: r['title'], comment: r['disambiguation'] }); }
 function formatWorkLink(r) { return linkTemplate.expand({ url: '?work=' + r['id'], text: r['title'] }); }
 
 var coverArtTemplate = jsontemplate.Template(
 	'<div class="cover-art">' +
-		'<img src="{url}" onerror="coverArtMissing(this);" alt="">' +
+		'<img class="img-responsive" src="{url}" onerror="coverArtMissing(this);" alt="">' +
 	'</div>'
 );
 
