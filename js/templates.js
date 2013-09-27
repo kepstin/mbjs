9d7a5fdf6e80d85c73e4a44fa324f5998b41522b
Calvin Walton <calvin.walton@kepstin.ca>
2013-09-27 08:34:22 -0400

    Use if so nested checks work

--- js/templates.js
+++ js/templates.js
@@ -346,7 +346,7 @@ var releaseTileTemplate = jsontemplate.Template(
 					'{.section label}' +
 						'{@|label-link}' +
 					'{.end}' +
-					'{.section label}{.section catalog-number} – {.end}{.end}' +
+					'{.if label}{.if catalog-number} – {.end}{.end}' +
 					'{.section catalog-number}' +
 						'{@|html}' +
 					'{.end}' +
